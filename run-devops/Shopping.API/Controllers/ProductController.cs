14471f6f4b62a4b45ad3d6dfc426354dfddd4cfe
bferraz <b20-ferraz@hotmail.com>
2023-05-14 20:34:15 -0300

    codificação

---
+++ run-devops/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Models;
+using Shopping.Client.Data;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+    }
+}
